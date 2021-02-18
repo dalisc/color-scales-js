3037fa359b0be3c1b93fb8759866e7027bae9d0e
Dalis Chan <dalischan97@gmail.com>
2021-02-18 23:29:51 +0800

    Add validator for colorStop

--- src/validators.ts
+++ src/validators.ts
@@ -8,12 +8,11 @@ export function validateMinMaxValues(min: number, max: number) {
   }
 }
 
-export function validateMinMaxColors(minColor: string, maxColor: string) {
-  if (minColor.toUpperCase() === maxColor.toUpperCase()) {
-    throw new Error('The minimum and maximum colors cannot be equal.');
+export function validateColorStops(colorStops: string[]) {
+  if (colorStops.length < 2) {
+    throw new Error('At least two colors must be provided.');
   }
 }
-
 export function validateAlphaValue(alpha: number) {
   if (alpha < 0 || alpha > 1) {
     throw new Error('The alpha value must be between 0 and 1.');
